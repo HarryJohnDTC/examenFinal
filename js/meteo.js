93d0ab1b62fbe2c521e0cfe2948bad4f994ab62e
RANDRIANAVALONA Livarijaona Harena <geit.harena@gmail.com>
2024-06-01 23:29:38 +0300

    toogle meteo

--- js/meteo.js
+++ js/meteo.js
@@ -31,3 +31,11 @@ document.addEventListener('DOMContentLoaded', function() {
 
     fetchWeather();
 });
+document.addEventListener('DOMContentLoaded', function() {
+    const mobileMenuToggle = document.querySelector('#mobile-menu-toggle');
+    const mobileMenu = document.querySelector('#mobile-menu');
+
+    mobileMenuToggle.addEventListener('click', function() {
+      mobileMenu.classList.toggle('hidden');
+    });
+  });
